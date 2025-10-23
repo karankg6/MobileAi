debe10a855dc8fb8827307fe03c8c409dbe1eb08
karankg6 <161154719+karankg6@users.noreply.github.com>
2025-10-23 17:51:22 -0400

    Add files via upload

---
+++ MobileAI/composeApp/src/webMain/kotlin/org/kgajjar/mobileai/main.kt
@@ -0,0 +1,11 @@
+package org.kgajjar.mobileai
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
